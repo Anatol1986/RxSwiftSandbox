4c94a2ec97003af7e5fc33e6461928ad01b022d8
Anatoly Uarmolovich <anatoly.uarmolovich@gmail.com>
2020-03-04 13:06:31 +0300

    feat: some working maked

--- RxPlayground.playground/Contents.swift
+++ RxPlayground.playground/Contents.swift
@@ -2,6 +2,93 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-class somec {
-    
+//example(of: "just, of, from", action: {
+//    let one = 1
+//    let two = 2
+//    let three = 3
+//    let observable = Observable<Int>.just(one)
+//    let observable2 = Observable.of(one, two, three)
+//    let observable3 = Observable.of([one, two, three])
+//    let observable4 = Observable.from([one, two, three])
+//
+//})
+
+//example(of: "subscribe") {
+//      let one = 1
+//      let two = 2
+//      let three = 3
+//    let observable = Observable.of(one, two, three)
+//
+//    observable.subscribe { (event) in
+//
+//
+//        if let element = event.element {
+//            print(element)
+//        }
+//    }
+//    observable.subscribe(onNext: { (element) in
+//        print(element)
+//    })
+//
+//}
+//example(of: "empty") {
+//    let observable = Observable<Void>.empty()
+//    observable.subscribe(onNext: { (element) in
+//        print(element)
+//    }, onCompleted: {
+//        print("Completed")
+//    })
+//}
+
+
+//example(of: "never") {
+//    let observable = Observable<Any>.never()
+//    observable.subscribe(onNext: { (element) in
+//        print(element)
+//    }, onCompleted: {
+//        print("Complete")
+//    })
+//}
+
+//example(of: "range") {
+//    let observable = Observable<Int>.range(start: 1, count: 10)
+//    observable.subscribe(onNext: { i in
+//        let n = Double(i)
+//        let fibonacci = Int(((pow(1.61803, n) - pow(0.61803, n)) / 2.23606).rounded())
+//        print(fibonacci)
+//    })
+//}
+
+//example(of: "dispose") {
+//    let observable = Observable.of("A", "B", "C")
+//    let subscribtion = observable.subscribe { (event) in
+//        print(event)
+//    }
+//    subscribtion.dispose()
+//}
+//example(of: "DisposeBag") {
+//    let disposeBag = DisposeBag()
+//    Observable.of("A", "B", "C").subscribe { print($0)
+//    }.disposed(by: disposeBag)
+//}
+
+example(of: "create") {
+    let disposeBag = DisposeBag()
+    Observable<String>.create {  observer in
+        observer.onNext("1")
+        observer.onCompleted()
+        observer.onNext("?")
+        return Disposables.create()
+    }
 }
+
+
+
+
+
+
+
+
+
+//example(of: <#T##String#>, action: <#T##() -> Void#>)
+//example(of: <#T##String#>, action: <#T##() -> Void#>)
