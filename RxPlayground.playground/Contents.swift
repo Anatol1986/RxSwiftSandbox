8ffe5944daae294517518ac4e5a77ec4edf60574
Anatoly Uarmolovich <anatoly.uarmolovich@gmail.com>
2020-03-04 17:34:34 +0300

    feat: Continuing testing and learning RxSwift framework

--- RxPlayground.playground/Contents.swift
+++ RxPlayground.playground/Contents.swift
@@ -72,22 +72,161 @@ import RxCocoa
 //    }.disposed(by: disposeBag)
 //}
 
-example(of: "create") {
-    let disposeBag = DisposeBag()
-    Observable<String>.create {  observer in
-        observer.onNext("1")
-        observer.onCompleted()
-        observer.onNext("?")
-        return Disposables.create()
-    }
-}
 
+////MARK: - MEMORY LEAK LOWER BY TEXT
+//example(of: "create") {
+//
+//    enum MyError: Error {
+//      case anError
+//    }
+//
+//    let disposeBag = DisposeBag()
+//    Observable<String>.create {  observer in
+//        observer.onNext("1")
+////        observer.onError(MyError.anError)
+////        observer.onCompleted()
+//        observer.onNext("?")
+//        return Disposables.create()
+//    }.subscribe(onNext: {
+//        print($0)
+//    }, onError: { print($0)
+//    }, onCompleted: {
+//        print("Completed")
+//    }) {
+//        print("Disposed")
+//    }
+////    .disposed(by: disposeBag)
+//}
 
+//example(of: "deffered") {
+//    let disposeBag = DisposeBag()
+//    var flip: Bool = false
+//
+//    let factory: Observable<Int> = Observable.deferred {
+//
+//        flip.toggle()
+//
+//        if flip {
+//            return Observable.of(1, 2, 3)
+//        } else {
+//            return Observable.of(4,5,6)
+//        }
+//
+//    }
+//
+//    for _ in 0...3 {
+//        factory.subscribe(onNext: {
+//            print($0, terminator: "")
+//
+//            }) .disposed(by: disposeBag)
+//          print()
+//    }
+//
+//}
+// MARK: -- TRAITS ("light version of Observables")
 
+//example(of: "Single") {
+//    let disposeBag = DisposeBag()
+//
+//    enum FileReadError: Error {
+//        case fileNotFound, unreadable, encodingFailed
+//    }
+//
+//    func loadText(from name: String) -> Single<String> {
+//        return Single.create {  single  in
+//            let disposable = Disposables.create()
+//            guard let path = Bundle.main.path(forResource: name, ofType: "txt") else {
+//                single(.error(FileReadError.fileNotFound))
+//                return disposable
+//            }
+//            guard let data = FileManager.default.contents(atPath: path) else {
+//                single(.error(FileReadError.unreadable))
+//                return disposable
+//            }
+//
+//            guard let contents = String(data: data, encoding: .utf8) else {
+//                single(.error(FileReadError.encodingFailed))
+//                return disposable
+//            }
+//            single(.success(contents))
+//            return disposable
+//        }
+//    }
+//
+//    loadText(from: "Copyright").subscribe {
+//
+//        switch $0 {
+//        case .success(let string): print(string)
+//        case .error(let error): print(error)
+//        }
+//    }.disposed(by: disposeBag)
+//}
 
+// MARK: -- Subjects
 
+//example(of: "Publish subject") {
+//    let subject = PublishSubject<String>()
+//    subject.onNext("Is anyone listening?")
+//
+//    let subscriptionOne = subject.subscribe(onNext: { string in
+//        print(string)
+//    })
+//
+//    subject.on(.next("1"))
+//    subject.onNext("2")
+//
+//    let subscriptionTwo = subject.subscribe { (event) in
+//        print("2)", event.element ?? event)
+//
+//    }
+//
+//    subject.onNext("3")
+//
+//    subscriptionOne.dispose()
+//
+//    subject.onNext("4")
+//    subject.onCompleted()
+//    subject.onNext("5")
+//    subscriptionTwo.dispose()
+//
+//    let disposeBag = DisposeBag()
+//
+//    subject.subscribe {
+//        print("3)", $0.element ?? $0)
+//    }.disposed(by: disposeBag)
+//    subject.onNext("?")
+//
+//}
 
+// MARK: -- Behavior Subjects
+//enum MyError: Error {
+//    case anError
+//}
+//func printCustom<T: CustomStringConvertible>(label: String, event: Event<T>) {
+//    print(label, (event.element ?? event.error) ?? event)
+//}
+//
+//example(of: "Behavior Subject") {
+//    let subject = BehaviorSubject(value: "Initial value")
+//    let disposeBag = DisposeBag()
+//
+//    subject.onNext("X")
+//
+//    subject.subscribe {
+//        printCustom(label: "1)", event: $0)
+//    }.disposed(by: disposeBag)
+//
+//    subject.onError(MyError.anError)
+//    subject.subscribe {
+//        printCustom(label: "2)", event: $0)
+//    }.disposed(by: disposeBag)
+//
+//
+//}
 
+//MARK: --REPLAY SUBJECT
+
+//example(of: <#T##String#>, action: <#T##() -> Void#>)
 
 
 //example(of: <#T##String#>, action: <#T##() -> Void#>)
